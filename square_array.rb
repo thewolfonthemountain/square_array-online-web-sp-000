1438d71e2b112226642e5ba7b778ca75444a662a
Sean Langton <slangton1@gmail.com>
2018-10-26 20:37:56 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  array_size = array.size
+  numbers_squared = 0
+  new_numbers = []
+  do while numbers_squared < array_size
+    array[numbers_squared]*array[numbers_squared] << new_numbers
+    numbers_squared += 1
 end
